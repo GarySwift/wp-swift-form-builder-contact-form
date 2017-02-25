7bf8e7f1fab117d7e9a44e8f5beaf6e68d08bb23
Gary Swift <garyswiftmail@gmail.com>
2017-02-25 09:10:00 +0000

    Changed form processing

--- form-builder-contact-form.php
+++ form-builder-contact-form.php
@@ -8,12 +8,12 @@ License:           GPL-2.0+
 Text Domain:       wp-swift-form-builder-contact-form
 */
 class WP_Swift_Form_Builder_Contact_Form_Plugin {
-     // private $Form_Builder = null;
+    private $attributes = null;
     /*
      * Initializes the plugin.
      */
     public function __construct() {
-                # A shortcode for rendering the contact form.
+        # A shortcode for rendering the contact form.
         add_shortcode( 'custom-contact-form', array( $this, 'render_contact_form' ) ); 
         // add_action( 'wp_enqueue_scripts', array($this, 'enqueue_javascript') );
     }
@@ -33,43 +33,18 @@ class WP_Swift_Form_Builder_Contact_Form_Plugin {
         $Form_Builder = null;
         if ( class_exists('WP_Swift_Form_Builder_Plugin')) {
             $submit_button_name = "submit-request-form";
-            $form_data = $this->get_account_form();
+            $form_data = $this->get_form_data();
             $form_builder_args = array(
                 "form_name" => "request-form",
                 "submit_button_name" => $submit_button_name,
                 "button_text" => "Submit Query",
+                "show_mail_receipt" => true,
             );
-            // return __( 'Page ID: '.get_the_ID(), 'wp-swift-form-builder-contact-form' );
-            $Form_Builder = new WP_Swift_Form_Builder_Plugin($form_data, get_the_ID(), $form_builder_args, false);
-            if ($Form_Builder != null ) {
-                $Form_Builder->set_form_data($form_data, get_the_ID(), $form_builder_args, false); //"option"
-            }
+            $Form_Builder = new WP_Swift_Form_Builder_Plugin($attributes, $form_data, get_the_ID(), $form_builder_args, $attributes, false);
         }
         else {
             return __( '<h4>Please install plugin WP Swift: Form Builder</h4>', 'wp-swift-form-builder-contact-form' );
         }
-        // Parse shortcode attributes
-        // $default_attributes = array( 'show_title' => false);//524 //, 'redirect' => '524' 
-        // $attributes = shortcode_atts( $default_attributes, $attributes );
-        // $show_title = $attributes['show_title'];
-     
-         
-        // Error messages
-        // $errors = array();
-        // if ( isset( $_REQUEST['contact'] ) ) {
-        //     $error_codes = explode( ',', $_REQUEST['contact'] );
-         
-        //     foreach ( $error_codes as $code ) {
-        //         $errors []= $this->get_error_message( $code );
-        //     }
-        // }
-        // $attributes['errors'] = $errors;   
-
-        // Check if user just logged out
-        // $attributes['logged_out'] = isset( $_REQUEST['logged_out'] ) && $_REQUEST['logged_out'] == true;
-
-
-        
         // Render the contact form using an external template
         return $this->get_template_html( 'contact_form', $attributes, $Form_Builder );
     }
@@ -88,68 +63,23 @@ class WP_Swift_Form_Builder_Contact_Form_Plugin {
         }
      
         ob_start();
-     
-     // echo "<pre>"; echo $this->form_builder->add(2, 2); echo "</pre>";
-        do_action( 'custom_contact_before_' . $template_name );
+        do_action( 'custom_contact_before_' . $template_name );//To do
 
-// echo "<pre>"; var_dump($Form_Builder->form_settings); echo "</pre>";
         // If POST
-if(isset($_POST['submit-request-form'])){ //check if form was submitted
+        if(isset($_POST['submit-request-form'])){ //check if the form was submitted
 
-  $Form_Builder->set_form_pristine(false);
-    // echo "<pre>"; var_dump($_POST); echo "</pre><hr>";
-    // $form_settings = process_form($form_settings, $_POST);
-
-
-        //      $mail_receipt=false;//auto-reponse flag
-        // if(isset($post['mail-receipt'])){
-        //     $mail_receipt=true;//Send an auto-response to user
-        // }
-        // // include('_email-template.php');
-    // echo "<pre>"; var_dump($Form_Builder->form_settings); echo "</pre><hr>";
-        //Loop through the POST and validate. Store the values in $form_data
-        foreach ($_POST as $key => $value) {
-            if (($key!='submit-request-form') && ($key!='mail-receipt') && ($key!='form-file-upload') && ($key!='g-recaptcha-response')) { //Skip the button and mail-receipt checkbox
-                // $Form_Builder->form_settings[$key] = check_input($Form_Builder->form_settings[$key], $value);//Validate input  
-
-                // check_input($data, $value)
-
-                // $Form_Builder->form_settings[$key] = $Form_Builder->check_input($Form_Builder->form_settings[$key], $value);//Validate input    
-                $Form_Builder->check_input($key, $value);//Validate input    
-                // echo '<pre>'.$key.': ';var_dump($Form_Builder->form_settings["form_data"][$key]); echo '</pre><hr>';
+            $Form_Builder->validate_form();
+            if ($Form_Builder->get_error_count()==0) {
+                echo $this->process_successful_form($Form_Builder, $_POST);
             }
-        }
-         // echo "<pre>"; var_dump($Form_Builder->form_settings); echo "</pre><hr>";
-}   
+        }   
 
-     
-
-
-
-
-        // // Loop through form1_data and increase form1_num_error_found count for each error
-        // foreach ($form_settings["form_data"] as $key => $value) {
-        //     if(!$form_settings["form_data"][$key]['passed']) {
-        //         //An error has been found in this input so increase the count
-        //         $form_settings["form_num_error_found"]++;
-        //     }
-        // }
-
-        // if($form_settings["form_num_error_found"]) {
-        //     // Error has been found in user input
-        //     $form_settings["response_msg"] = "We're sorry, there has been an error with the form input.<br>Please rectify the ".$form_num_error_found." errors below and resubmit.";
-        //     $form_settings["error_class"] = 'error';
-        //     // echo "<pre>"; var_dump($form_settings); echo "</pre>";
-        // }
-
-
-
-
-        // require( 'templates/' . $template_name . '.php');
+        /*
+         * Look for form
+         */
         if ($Form_Builder != null ) {
             $Form_Builder->acf_build_form();
         }
-
      
         do_action( 'custom_contact_after_' . $template_name );
      
@@ -159,6 +89,154 @@ if(isset($_POST['submit-request-form'])){ //check if form was submitted
         return $html;
     }
 
+/*
+ * The form has been validated so we can send the emails.
+ * This will get the to and from email recipients, build the html message and send the emails.
+ * It then returns a html string that tells the user what has happened.
+ *
+ * @param class $Form_Builder   The name of the template to render (without .php)
+ * @param array  $post          The global $_POST variable cast as a the local $post variable
+ *
+ * @return string               The html success message
+ */
+private function process_successful_form($Form_Builder, $post, $post_id=false) {
+    /*
+     * Variables
+     */
+    $send_email=true;//Debug variable. If false, emails will not be sent
+    $date = ' - '.date("Y-m-d H:i:s").' GMT';
+    $post_id_or_acf_option= '';//We can specify if it is an option field or use a post_id (https://www.advancedcustomfields.com/add-ons/options-page/)
+    $mail_receipt=false;//auto-reponse flag
+    if(isset($post['mail-receipt'])){
+        $mail_receipt=true;//Send an auto-response to user
+    }
+
+    /*
+     * These are the default form settings
+     */
+    // If a debug email is set in ACF, send the email there instead of the admin email
+    $to = get_option('admin_email');
+    // Set reponse subject for email
+    $response_subject = "New Enquiry".$date;
+    // Start the reponse message for the email
+    $response_message = '<p>A website user has made the following enquiry.</p>';
+    //Set auto_response_message
+    $auto_response_message = 'Thank you very much for your enquiry. A representative will be contacting you shortly.';
+    // Set the response that is set back to the browser
+    $browser_output_header = 'Hold Tight, We\'ll Get Back To You';
+    // The auto-response subject
+    $auto_response_subject='Auto-response (no-reply)';
+
+    /*
+     * Now, we can override the default settings if they are set
+     */
+    // If a debug email is set in ACF, send the email there instead of the admin email
+    if (get_field('debug_email', $post_id_or_acf_option)) {
+        $to = get_field('debug_email', $post_id_or_acf_option); 
+    }
+    // Set reponse subject for email
+    if (get_field('response_subject', $post_id_or_acf_option)) {
+        $response_subject = get_field('response_subject', $post_id_or_acf_option).$date; 
+    }
+    // Start the reponse message for the email
+    if (get_field('response_message', $post_id_or_acf_option)) {
+        $response_message = '<p>'.get_field('response_message', $post_id_or_acf_option).'.</p>';
+    }
+    //Set auto_response_message
+    if (get_field('auto_response_message', $post_id_or_acf_option)) {
+        $auto_response_message = get_field('auto_response_message', $post_id_or_acf_option);
+    }
+    // Set the response that is set back to the browser
+    if (get_field('browser_output_header', $post_id_or_acf_option)) {
+        $browser_output_header = get_field('browser_output_header', $post_id_or_acf_option);
+    } 
+    // The auto-response subject
+    if( get_field('auto_response_subject') ) {
+        $auto_response_subject = get_field('auto_response_subject');
+    }
+
+    // Start making the string that will be sent in the email
+    $email_string = $response_message;
+    $key_value_table = $this->build_key_value_table($Form_Builder);
+    // Add the table of values to the string
+    $email_string .= $key_value_table;
+
+    /*
+     * Send the email to the admin/office
+     */
+    if ($send_email) {
+        $status = wp_mail($to, $response_subject.' - '.date("D j M Y, H:i"). ' GMT',  $email_string);//wrap_email($email_string)
+    }
+    /*
+     * If the user has requested it, send an email acknowledgement
+     */
+    if($mail_receipt) {
+        $user_email_string = $auto_response_message.'<p>A copy of your enquiry is shown below.</p>'.$key_value_table;
+        if ($send_email) {
+            $status = wp_mail($Form_Builder->form_settings["form_data"]['form-email']['clean'], $auto_response_subject, $user_email_string);// wrap_email($user_response_msg)
+        }
+    }
+    /*
+     * Return the html
+     */              
+    return $this->build_confirmation_output($use_callout=true, $browser_output_header, $auto_response_message, $key_value_table);
+}
+
+private function build_key_value_table($Form_Builder) {
+ob_start(); 
+?>
+    <table style="width:100%">
+        <tbody>
+            <?php foreach ($Form_Builder->form_settings["form_data"] as $key => $value): ?>
+                <?php                 
+                    $required = $value['required'];
+                    $type = $value['type'];
+                ?>
+                <tr>
+                    <th style="width:30%"><?php echo ucwords(str_replace('-', ' ',substr($key, 5))) ?></th>
+                    <td>
+                        <?php 
+                            if ($value['type']=='select') {
+                                echo ucwords(str_replace('-', ' ',$value['clean']));
+                            } else {
+                                echo $value['clean'];
+                            }
+                        ?>
+                    </td>
+                </tr>
+               
+            <?php endforeach ?>   
+        </tbody>
+    </table>      
+<?php
+$html = ob_get_contents();
+ob_end_clean();
+return $html;
+}
+
+private function build_confirmation_output($use_callout=true, $browser_output_header, $auto_response_message, $key_value_table) {
+ob_start(); ?>
+    <?php if ($use_callout): ?>
+        <div id="contact-thank-you">
+            <div class="callout secondary" data-closable="slide-out-right">        
+    <?php endif ?>
+
+            <h3><?php echo $browser_output_header; ?></h3>
+            <p><?php echo $auto_response_message; ?></p>
+            <p>A copy of your enquiry is shown below.</p>
+            <?php echo $key_value_table; ?>
+
+    <?php if ($use_callout): ?>
+                <button class="close-button" aria-label="Dismiss alert" type="button" data-close>
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>        
+    <?php endif;
+$html = ob_get_contents();
+ob_end_clean();
+return $html;
+}
     private function get_form_data() {
         return array (
         'form-name' => 
--- templates/contact_form.php
+++
@@ -1,4 +0,0 @@
-<?php
-if ($Form_Builder != null ) {
-    $Form_Builder->acf_build_form();
-}
